df0bb07d400675e0d0500d5c23c05a81ff8aef35
LitvinovKV <SquertelSquertel@mail.ru>
2025-02-08 12:28:31 +0300

    completed the Cars Assembly task (#5)

---
+++ CarsAssemble/cars_assemble.go
@@ -0,0 +1,17 @@
+package cars
+
+const CountOfCarsInGroup = 10
+const PriceOfIndividuallyCar = 10000
+const PriceOfCarGroup = 95000
+
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / float64(100))
+}
+
+func CalcualteWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(float64(productionRate/60) * (successRate / float64(100)))
+}
+
+func CalculateCost(carsCount int) uint {
+	return uint(((carsCount / CountOfCarsInGroup) * PriceOfCarGroup) + ((carsCount % CountOfCarsInGroup) * PriceOfIndividuallyCar))
+}
